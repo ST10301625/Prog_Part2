799f47498c51ab7360abeed763ee0c88e326bef8
ST10301625 <128587661+ST10301625@users.noreply.github.com>
2024-10-18 11:41:47 +0200

    Added attribution

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,7 +18,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using NUnit.Framework;
 
+
+//Digital TechJoint(2022). ASP.NET MVC - How To Implement Role Based Authorization.
+//YouTube. Available at: https://www.youtube.com/watch?v=qvsWwwq2ynE. [Accessed 10 October]
+//The following code was created with the help of this video ‌
 namespace Prog2bPOEPart2.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
@@ -45,35 +50,20 @@ namespace Prog2bPOEPart2.Areas.Identity.Pages.Account
             _emailSender = emailSender;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+       
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+       
         public string ReturnUrl { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+        
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
+       
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+          
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -83,20 +73,13 @@ namespace Prog2bPOEPart2.Areas.Identity.Pages.Account
             [Display(Name = "Position")]
             public string Position { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+           
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using Prog2bPOEPart2.Models;
 
-namespace Prog2bPOEPart2.Data
+
+//tutorialsEU - C# (2023). CREATE and CONNECT DATABASES in ASP.NET. [online] YouTube. 
+//   Available at: https://www.youtube.com/watch?v=ZX12X-ALwGA [Accessed 11 Oct. 2024].
+// The tutorial above shows how to connect a database to mvc using ssms‌
+
+
+ namespace Prog2bPOEPart2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
